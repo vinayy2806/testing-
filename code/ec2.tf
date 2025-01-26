bc2432d16ae2b2a46582e8b45f5c5d984459794d
VINAY Y <vinay28623@gmail.com>
2025-01-26 13:01:01 +0530

    Update ec2.tf

--- code/ec2.tf
+++ code/ec2.tf
@@ -1,13 +1,13 @@
 provider "aws" {
     region = "us-east-1"
-    # this key are take for the test IAM user
+    # This key are taken for the test IAM user
     
     access_key = "AKIASE5KRGE4UYBEGBET"
     secret_key = "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"
 }
 
 resource "aws_instance" "admin" {
-    ami = "ami-04b4f1a9cf54c11d0"
+    Ami = "ami-04b4f1a9cf54c11d0"
     instance_type = "t2.medium"
     security_groups = [ "default" ]
     key_name = "test"
